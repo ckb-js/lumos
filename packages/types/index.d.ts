40427e7b5fe177f0f2514a61644486ff9ab3d292
classicalliu <classicalliu@gmail.com>
2020-06-11 20:06:42 +0800

    chore: Add some type definitions for types/utils

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,1 +1,17 @@
 import * as core from "./lib/core";
+
+export declare const utils: {
+  /**
+   * convert bigint to BigUInt64 little-endian hex string
+   *
+   * @param num bigint or number type can using BigInt() to convert to bigint
+   */
+  toBigUInt64LE(num: bigint | string): string;
+
+  /**
+   * convert BigUInt64 little-endian hex string to bigint
+   *
+   * @param hex BigUInt64 little-endian hex string
+   */
+  readBigUInt64LE(hex: string): bigint;
+};
