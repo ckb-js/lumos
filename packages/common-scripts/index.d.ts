571c15d60b70a45c40c3f46d6df1a1682233ac20
classicalliu <classicalliu@gmail.com>
2020-06-11 19:07:25 +0800

    chore: Add types definition for dao and secp256k1Blake160

--- packages/common-scripts/index.d.ts
+++ packages/common-scripts/index.d.ts
@@ -1,7 +1,9 @@
-import { TransactionSkeleton } from "@ckb-lumos/types"
+import { TransactionSkeleton } from "@ckb-lumos/helpers"
 
 export type Address = string
 export type Config = any // TODO: define this type later
+export type FullCell = any
+export type CellProvider = any
 
 /**
  * secp256k1_blake160_multisig script requires S, R, M, N and public key hashes
@@ -22,6 +24,57 @@ export type FromInfo = MultisigScript | Address
 export type ToInfo = Address | number
 
 // TODO: secp256k1Blake160 types
+export declare const secp256k1Blake160: {
+  /**
+   * transfer capacity from secp256k1_blake160 script cells
+   *
+   * @param txSkeleton
+   * @param fromAddress
+   * @param toInfo address or outputIndex, can be any type of lock script and can left empty. Notice if it's output index, will change capacity even this output is fixed by `fixedEntries`.
+   * @param amount transfer CKB capacity in shannon
+   * @param options
+   */
+  transfer(
+    txSkeleton: TransactionSkeleton,
+    fromAddress: Address,
+    toInfo: ToInfo,
+    amount: bigint,
+    options: {
+      config: Config,
+      requireToAddress: boolean,
+    },
+  ): TransactionSkeleton,
+
+  /**
+   * pay fee by secp256k1_blake160 script cells
+   *
+   * @param txSkeleton
+   * @param fromAddress
+   * @param amount fee in shannon
+   * @param options
+   */
+  payFee(
+    txSkeleton: TransactionSkeleton,
+    fromAddress: Address,
+    amount: bigint,
+    options: {
+      config: Config,
+    },
+  ): TransactionSkeleton,
+
+  /**
+   * prepare for txSkeleton signingEntries, will update txSkeleton.get("signingEntries")
+   *
+   * @param txSkeleton
+   * @param options
+   */
+  prepareSigningEntries(
+    txSkeleton: TransactionSkeleton,
+    options: {
+      config: Config,
+    },
+  ): TransactionSkeleton,
+}
 
 export declare const secp256k1Blake160Multisig: {
   /**
@@ -87,4 +140,72 @@ export declare const secp256k1Blake160Multisig: {
    * @returns lock script args
    */
   multisigArgs(serializedMultisigScript: string): string,
+}
+
+export declare const dao: {
+  /**
+   * deposit a cell to DAO
+   *
+   * @param txSkeleton
+   * @param toAddress deposit cell lock address
+   * @param options
+   */
+  deposit(
+    txSkeleton: TransactionSkeleton,
+    toAddress: Address,
+    options: {
+      config: Config,
+    },
+  ): TransactionSkeleton,
+
+  /**
+   * list DAO cells,
+   *
+   * @param cellProvider
+   * @param fromAddress
+   * @param cellType "deposit" or "withdraw", can using "" to list all.
+   * @param options
+   */
+  listDaoCells(
+    cellProvider: CellProvider,
+    fromAddress: Address,
+    cellType: string,
+    options: {
+      config: Config,
+    },
+  ): List<FullCell>,
+
+  /**
+   * withdraw an deposited DAO cell
+   *
+   * @param txSkeleton
+   * @param fromInput deposited DAO cell
+   * @param options
+   */
+  withdraw(
+    txSkeleton: TransactionSkeleton,
+    fromInput: FullCell,
+    options: {
+      config: Config,
+    },
+  ): TransactionSkeleton,
+
+  /**
+   * Unlock a withdrew DAO cell
+   *
+   * @param txSkeleton
+   * @param depositInput deposited DAO cell
+   * @param withdrawInput withdrew DAO cell
+   * @param toAddress
+   * @param options
+   */
+  unlock(
+    txSkeleton: TransactionSkeleton,
+    depositInput: FullCell,
+    withdrawInput: FullCell,
+    toAddress: Address,
+    options: {
+      config: Config,
+    },
+  ): TransactionSkeleton,
 }
---
+++ packages/helpers/index.d.ts
@@ -0,0 +1,1 @@
+export { TransactionSkeleton } from "./lib";
