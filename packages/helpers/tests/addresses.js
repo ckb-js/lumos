e67d32f9a226f93597b8d98f99d3a2407ed6f40d
classicalliu <classicalliu@gmail.com>
2020-06-13 20:12:11 +0800

    test: Add some basic tests for helpers

---
+++ packages/helpers/tests/addresses.js
@@ -0,0 +1,54 @@
+const shortAddressInfo = {
+  testnetAddress: "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83",
+  mainnetAddress: "ckb1qyqrdsefa43s6m882pcj53m4gdnj4k440axqdt9rtd",
+  script: {
+    code_hash:
+      "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+    hash_type: "type",
+    args: "0x36c329ed630d6ce750712a477543672adab57f4c",
+  },
+};
+
+const multisigAddressInfo = {
+  mainnetAddress: "ckb1qyq5lv479ewscx3ms620sv34pgeuz6zagaaqklhtgg",
+  testnetAddress: "ckt1qyq5lv479ewscx3ms620sv34pgeuz6zagaaqt6f5y5",
+  script: {
+    code_hash:
+      "0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8",
+    hash_type: "type",
+    args: "0x4fb2be2e5d0c1a3b8694f832350a33c1685d477a",
+  },
+};
+
+const fullAddressInfo = {
+  mainnetAddress:
+    "ckb1qsqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqpvumhs9nvu786dj9p0q5elx66t24n3kxgmz0sxt",
+  testnetAddress:
+    "ckt1qsqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqpvumhs9nvu786dj9p0q5elx66t24n3kxgkpkap5",
+  script: {
+    code_hash:
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+    hash_type: "type",
+    args: "0xb39bbc0b3673c7d36450bc14cfcdad2d559c6c64",
+  },
+};
+
+const fullAddressInfoWithData = {
+  mainnetAddress:
+    "ckb1q2da0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsdkr98kkxrtvuag8z2j8w4pkw2k6k4l5c7jxc4f",
+  testnetAddress:
+    "ckt1q2da0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsdkr98kkxrtvuag8z2j8w4pkw2k6k4l5cn3l4jk",
+  script: {
+    code_hash:
+      "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+    hash_type: "data",
+    args: "0x36c329ed630d6ce750712a477543672adab57f4c",
+  },
+};
+
+module.exports = {
+  shortAddressInfo,
+  multisigAddressInfo,
+  fullAddressInfo,
+  fullAddressInfoWithData,
+};
---
+++ packages/helpers/tests/parse_address.test.js
@@ -0,0 +1,60 @@
+const test = require("ava");
+const { configs, parseAddress } = require("../lib");
+
+const {
+  shortAddressInfo,
+  multisigAddressInfo: multisigAddressInfo,
+  fullAddressInfo,
+} = require("./addresses");
+
+const { LINA, AGGRON4 } = configs;
+
+test("short address, mainnet", (t) => {
+  const script = parseAddress(shortAddressInfo.mainnetAddress, {
+    config: LINA,
+  });
+
+  t.deepEqual(script, shortAddressInfo.script);
+});
+
+test("short address, testnet", (t) => {
+  const script = parseAddress(shortAddressInfo.testnetAddress, {
+    config: AGGRON4,
+  });
+
+  t.deepEqual(script, shortAddressInfo.script);
+});
+
+test("short address, config not match address version", (t) => {
+  t.throws(() => {
+    parseAddress(shortAddressInfo.testnetAddress, { config: LINA });
+  });
+});
+
+test("multisig address, mainnet", (t) => {
+  const script = parseAddress(multisigAddressInfo.mainnetAddress);
+
+  t.deepEqual(script, multisigAddressInfo.script);
+});
+
+test("multisig address, testnet", (t) => {
+  const script = parseAddress(multisigAddressInfo.testnetAddress, {
+    config: AGGRON4,
+  });
+
+  t.deepEqual(script, multisigAddressInfo.script);
+});
+
+test("full address, mainnet", (t) => {
+  const script = parseAddress(fullAddressInfo.mainnetAddress);
+
+  t.deepEqual(script, fullAddressInfo.script);
+});
+
+test("full address, testnet", (t) => {
+  const script = parseAddress(fullAddressInfo.testnetAddress, {
+    config: AGGRON4,
+  });
+
+  t.deepEqual(script, fullAddressInfo.script);
+});
