6e80d1d797a54d9cc4ce812aa3ee110e449d1a82
classicalliu <classicalliu@gmail.com>
2020-06-22 18:05:22 +0800

    test: Test _commonTransfer

---
+++ packages/common-scripts/tests/common.test.js
@@ -0,0 +1,117 @@
+const test = require("ava");
+
+const { __tests__ } = require("../lib/common");
+const { _commonTransfer } = __tests__;
+const { DEV_CONFIG } = require("./dev_config");
+const { CellProvider } = require("./cell_provider");
+const { TransactionSkeleton } = require("@ckb-lumos/helpers");
+
+const aliceInputs = [
+  {
+    cell_output: {
+      capacity: "0x1d1a3543f00",
+      lock: {
+        code_hash:
+          "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
+        hash_type: "type",
+        args: "0xe2193df51d78411601796b35b17b4f8f2cd85bd0",
+      },
+    },
+    out_point: {
+      tx_hash:
+        "0x42300d78faea694e0e1c2316de091964a0d976a4ed27775597bad2d43a3e17da",
+      index: "0x1",
+    },
+    block_hash:
+      "0x156ecda80550b6664e5d745b6277c0ae56009681389dcc8f1565d815633ae906",
+    block_number: "0x1929c",
+    data: "0x",
+  },
+];
+
+const multisigInputs = [
+  {
+    cell_output: {
+      capacity: "0xba37cb7e00",
+      lock: {
+        code_hash:
+          "0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8",
+        hash_type: "type",
+        args: "0x56f281b3d4bb5fc73c751714af0bf78eb8aba0d8",
+      },
+    },
+    out_point: {
+      tx_hash:
+        "0xc0018c999d6e7d1f830ea645d980a3a9c3c3832d12e72172708ce8461fc5821e",
+      index: "0x1",
+    },
+    block_hash:
+      "0x29c8f7d773ccd74724f95f562d049182c2461dd7459ebfc494b7bb0857e8c902",
+    block_number: "0x1aed9",
+    data: "0x",
+  },
+];
+
+const cellProvider = new CellProvider(aliceInputs.concat(multisigInputs));
+let txSkeleton = TransactionSkeleton({ cellProvider });
+
+const bobAddress = "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83";
+const aliceAddress = "ckt1qyqwyxfa75whssgkq9ukkdd30d8c7txct0gqfvmy2v";
+
+const fromInfo = {
+  R: 0,
+  M: 1,
+  publicKeyHashes: ["0x36c329ed630d6ce750712a477543672adab57f4c"],
+};
+
+test("_commonTransfer, only alice", async (t) => {
+  const result = await _commonTransfer(
+    txSkeleton,
+    [aliceAddress],
+    bobAddress,
+    20000n * 10n ** 8n,
+    { config: DEV_CONFIG }
+  );
+  txSkeleton = result[0];
+  const amount = result[1];
+
+  const inputCapacity = txSkeleton
+    .get("inputs")
+    .map((i) => BigInt(i.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  const outputCapacity = txSkeleton
+    .get("outputs")
+    .map((o) => BigInt(o.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  t.is(txSkeleton.get("inputs").size, 1);
+  t.is(amount, 10n ** 8n);
+  t.is(outputCapacity - inputCapacity, amount);
+});
+
+test("_commonTransfer, alice and fromInfo", async (t) => {
+  const result = await _commonTransfer(
+    txSkeleton,
+    [aliceAddress, fromInfo],
+    bobAddress,
+    20000n * 10n ** 8n,
+    { config: DEV_CONFIG }
+  );
+  txSkeleton = result[0];
+  const amount = result[1];
+
+  const inputCapacity = txSkeleton
+    .get("inputs")
+    .map((i) => BigInt(i.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  const outputCapacity = txSkeleton
+    .get("outputs")
+    .map((o) => BigInt(o.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  t.is(txSkeleton.get("inputs").size, 2);
+  t.is(amount, 0n);
+  t.is(outputCapacity - inputCapacity, amount);
+});
--- packages/common-scripts/tests/locktime_pool.test.js
+++ packages/common-scripts/tests/locktime_pool.test.js
@@ -5,6 +5,7 @@ const { transfer, prepareSigningEntries, payFee } = locktimePool;
 const { CellProvider } = require("./cell_provider");
 const { calculateMaximumWithdraw } = require("../lib/dao");
 const { List } = require("immutable");
+const { DEV_CONFIG } = require("./dev_config");
 
 const inputInfos = [
   {
@@ -159,47 +160,6 @@ async function* cellCollector(_, fromScript) {
 const cellProvider = new CellProvider([]);
 let txSkeleton = TransactionSkeleton({ cellProvider });
 
-const devConfig = Object.assign(
-  {},
-  {
-    PREFIX: "ckt",
-    SCRIPTS: {
-      SECP256K1_BLAKE160: {
-        CODE_HASH:
-          "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-        HASH_TYPE: "type",
-        TX_HASH:
-          // "0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37",
-          "0x785aa819c8f9f8565a62f744685f8637c1b34886e57154e4e5a2ac7f225c7bf5",
-        INDEX: "0x0",
-        DEP_TYPE: "dep_group",
-        SHORT_ID: 0,
-      },
-      SECP256K1_BLAKE160_MULTISIG: {
-        CODE_HASH:
-          "0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8",
-        HASH_TYPE: "type",
-        TX_HASH:
-          // "0x6495cede8d500e4309218ae50bbcadb8f722f24cc7572dd2274f5876cb603e4e",
-          "0x785aa819c8f9f8565a62f744685f8637c1b34886e57154e4e5a2ac7f225c7bf5",
-        INDEX: "0x1",
-        DEP_TYPE: "dep_group",
-        SHORT_ID: 1,
-      },
-      DAO: {
-        CODE_HASH:
-          "0x82d76d1b75fe2fd9a27dfbaa65a039221a380d76c926f378d3f81cf3e7e13f2e",
-        HASH_TYPE: "type",
-        TX_HASH:
-          // "0x96fea0dfaac1186fbb98fd452cb9b13976f9a00bcce130035fe2e30dac931d1d",
-          "0x13c137fdf071c0ab3e6a4c8aaefc16c9bb7b9593b77822b151b18412ecd2ee41",
-        INDEX: "0x2",
-        DEP_TYPE: "code",
-      },
-    },
-  }
-);
-
 const tipHeader = {
   compact_target: "0x20010000",
   dao: "0x443110aefc4d1b55b10353894c2a29001e664c552fd16409005ef48f09d50200",
@@ -235,7 +195,7 @@ test("transfer multisig", async (t) => {
     bobAddress,
     BigInt(500 * 10 ** 8),
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
   // t.log(txSkeleton)
@@ -266,10 +226,10 @@ test("prepareSigningEntries, multisig", async (t) => {
     bobAddress,
     BigInt(500 * 10 ** 8),
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
-  txSkeleton = await prepareSigningEntries(txSkeleton, { config: devConfig });
+  txSkeleton = await prepareSigningEntries(txSkeleton, { config: DEV_CONFIG });
 
   t.is(txSkeleton.get("signingEntries").size, 1);
 
@@ -289,7 +249,7 @@ test("transfer multisig & dao", async (t) => {
     bobAddress,
     BigInt(2500 * 10 ** 8),
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
   // sum of outputs capacity should be equal to sum of inputs capacity
@@ -331,10 +291,10 @@ test("prepareSigningEntries, multisig & dao", async (t) => {
     bobAddress,
     BigInt(2500 * 10 ** 8),
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
-  txSkeleton = await prepareSigningEntries(txSkeleton, { config: devConfig });
+  txSkeleton = await prepareSigningEntries(txSkeleton, { config: DEV_CONFIG });
 
   t.is(txSkeleton.get("signingEntries").size, 3);
 
@@ -359,7 +319,7 @@ test("payFee, multisig & dao", async (t) => {
     bobAddress,
     BigInt(2500 * 10 ** 8),
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
   const fee = BigInt(1 * 10 ** 8);
@@ -368,7 +328,7 @@ test("payFee, multisig & dao", async (t) => {
     [fromInfo, aliceAddress],
     fee,
     tipHeader,
-    { config: devConfig, cellCollector }
+    { config: DEV_CONFIG, cellCollector }
   );
 
   const sumOfInputCapacity = txSkeleton
