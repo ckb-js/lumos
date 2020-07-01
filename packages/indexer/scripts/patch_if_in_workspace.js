5373132fa7cb2aeb79ca3642711332a76215e06c
Xuejie Xiao <xxuejie@gmail.com>
2020-07-01 02:13:13 +0000

    feat: Add support for multiple electron versions

--- packages/indexer/scripts/patch_if_in_workspace.js
+++ packages/indexer/scripts/patch_if_in_workspace.js
@@ -2,9 +2,9 @@
 // symlink_node_modules script. See `scripts/symlink_node_modules.js`
 // for more details.
 
-const spawn = require("child_process").spawn;
 const fs = require("fs");
 const path = require("path");
+const { exec } = require("./common");
 
 const file = path.resolve(
   path.join(__dirname, "../../../scripts/symlink_node_modules.js")
@@ -14,25 +14,4 @@ if (!fs.existsSync(file)) {
   return;
 }
 
-function exitWithError(data) {
-  console.error(data);
-  process.exit(1);
-}
-
-let stderr = "";
-spawn("node", [file], {
-  stdio: "inherit",
-  shell: true,
-})
-  .on("error", function (err) {
-    exitWithError(`Install binary error: ${err}, stderr: ${stderr}`);
-  })
-  .on("close", function (code) {
-    if (code !== 0 || stderr.indexOf("ERR") !== -1) {
-      exitWithError(`Install binary error, code: ${code}, stderr: ${stderr}`);
-    }
-    process.exit(0);
-  })
-  .on("data", function (data) {
-    stderr += String(data);
-  });
+exec("node", [file]);
