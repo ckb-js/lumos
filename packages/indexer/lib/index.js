40d04485b3a7645c52aea29b2f0bb0708bd0e2e7
classicalliu <classicalliu@gmail.com>
2020-07-31 15:02:35 +0800

    fix: Indexer cell collector type script

--- packages/indexer/lib/index.js
+++ packages/indexer/lib/index.js
@@ -124,7 +124,7 @@ class CellCollector {
       skip = null,
     } = {}
   ) {
-    if (!lock && !type) {
+    if (!lock && typeof type !== "object") {
       throw new Error("Either lock or type script must be provided!");
     }
     if (lock) {
@@ -164,7 +164,7 @@ class CellCollector {
       );
     }
 
-    if (this.type) {
+    if (this.type && typeof this.type === "object") {
       const scriptType = 1;
       const returnRawBuffer = true;
       typeOutPoints = new OrderedSet(
@@ -181,7 +181,7 @@ class CellCollector {
       );
     }
     let outPoints = null;
-    if (this.lock && this.type) {
+    if (this.lock && this.type && this.type !== "empty") {
       outPoints = lockOutPoints.intersect(typeOoutPoints);
     } else if (this.lock) {
       outPoints = lockOutPoints;
