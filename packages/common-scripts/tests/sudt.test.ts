bea156bc29e18adde9bde0810028014e3ae67e7a
classicalliu <classicalliu@gmail.com>
2020-09-22 17:40:11 +0800

    feat: Support anyone-can-pay destroyable in sudt transfer

--- packages/common-scripts/tests/sudt.test.ts
+++ packages/common-scripts/tests/sudt.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { sudt, anyoneCanPay } from "../src";
+import { sudt, anyoneCanPay, common } from "../src";
 import { CellProvider } from "./cell_provider";
 import {
   TransactionSkeletonType,
@@ -263,6 +263,74 @@ test("transfer acp", async (t) => {
 
   const expectedMessage =
     "0x12b4972ea24878f962e90c7cd0ab6019f7bd7629c85a8dfa33f66a272ea1bf0f";
+  t.is(txSkeleton.get("signingEntries").size, 1);
+  t.is(txSkeleton.get("signingEntries").get(0)!.message, expectedMessage);
+});
+
+test("transfer acp => secp, destroyable", async (t) => {
+  const cellProvider = new CellProvider([
+    ...bobAcpSudtInputs,
+    ...bobSecpInputs,
+  ]);
+  let txSkeleton: TransactionSkeletonType = TransactionSkeleton({
+    cellProvider,
+  });
+
+  const amount = BigInt(10000);
+  txSkeleton = await sudt.transfer(
+    txSkeleton,
+    [
+      {
+        address: bobAcpAddress,
+        destroyable: true,
+      },
+    ],
+    bob.secpLockHash,
+    alice.testnetAddress,
+    amount,
+    bobAcpAddress,
+    BigInt(1000 * 10 ** 8),
+    undefined,
+    { config: AGGRON4 }
+  );
+
+  txSkeleton = common.prepareSigningEntries(txSkeleton, {
+    config: AGGRON4,
+  });
+
+  const sumOfInputCapacity = txSkeleton
+    .get("inputs")
+    .map((i) => BigInt(i.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+  const sumOfOutputCapacity = txSkeleton
+    .get("outputs")
+    .map((o) => BigInt(o.cell_output.capacity))
+    .reduce((result, c) => result + c, BigInt(0));
+  t.is(sumOfOutputCapacity, sumOfInputCapacity);
+
+  const sumOfInputAmount = txSkeleton
+    .get("inputs")
+    .filter((i) => i.cell_output.type)
+    .map((i) => readBigUInt128LE(i.data))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  const sumOfOutputAmount = txSkeleton
+    .get("outputs")
+    .filter((i) => i.cell_output.type)
+    .map((i) => readBigUInt128LE(i.data))
+    .reduce((result, c) => result + c, BigInt(0));
+
+  t.is(sumOfInputAmount, sumOfOutputAmount);
+
+  t.is(txSkeleton.get("cellDeps").size, 2);
+  t.is(readBigUInt128LE(txSkeleton.get("outputs").get(0)!.data), amount);
+
+  t.true(
+    isSudtScript(txSkeleton.get("outputs").get(0)!.cell_output.type, AGGRON4)
+  );
+
+  const expectedMessage =
+    "0x69cba6e4676c52546a4cc04b01575da0d2f627badf797064ee3d815dc5387a36";
   t.is(txSkeleton.get("signingEntries").size, 1);
   t.is(txSkeleton.get("signingEntries").get(0)!.message, expectedMessage);
 });
