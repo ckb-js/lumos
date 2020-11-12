878f5feb027e1a995b49d4836f660e6bc0b2a82a
hupeng <bitrocks.hu@gmail.com>
2020-11-12 12:06:06 +0800

    feat: add HexNumber and jsdoc comment

--- packages/base/index.d.ts
+++ packages/base/index.d.ts
@@ -3,9 +3,16 @@ export { core };
 
 import { Reader } from "ckb-js-toolkit";
 
+/**
+ * HexString represents string starts with "0x" and followed by even number(including empty) of [0-9a-fA-F] characters.
+ */
 export type HexString = string;
+/**
+ * Hexadecimal represents string starts with "0x" and followed by any number(excluding empty) of [0-9a-fA-F] characters.
+ */
 export type Hexadecimal = string;
 export type Hash = HexString;
+export type HexNumber = Hexadecimal;
 export type PackedSince = string;
 export type PackedDao = string;
 
