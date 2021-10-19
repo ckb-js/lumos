4df351c2c10fa6051e3819853149ce6c1440c5af
wangximing <wangximing@cryptape.com>
2021-10-19 15:32:39 +0800

    feat: add deprecated annotation indexer and sql-indexer

--- packages/indexer/index.d.ts
+++ packages/indexer/index.d.ts
@@ -7,14 +7,23 @@ import {
   TransactionCollectorOptions,
 } from "@ckb-lumos/base";
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export declare class Indexer extends BaseIndexer {
   constructor(uri: string, path: string, options?: IndexerOptions);
 }
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export declare class CellCollector extends BaseCellCollector {
   constructor(indexer: Indexer, queries: QueryOptions);
 }
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export declare class TransactionCollector extends BaseTransactionCollector {
   constructor(
     indexer: Indexer,
--- packages/sql-indexer/index.d.ts
+++ packages/sql-indexer/index.d.ts
@@ -8,14 +8,23 @@ import {
   TransactionCollectorOptions,
 } from "@ckb-lumos/base";
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export class Indexer extends BaseIndexer {
   constructor(uri: string, knex: Knex, options?: IndexerOptions);
 }
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export declare class CellCollector extends BaseCellCollector {
   constructor(knex: Knex, queries: QueryOptions);
 }
 
+/**
+ * @deprecated since version 0.17.0-rc5
+ */
 export declare class TransactionCollector extends BaseTransactionCollector {
   constructor(
     indexer: Indexer,
