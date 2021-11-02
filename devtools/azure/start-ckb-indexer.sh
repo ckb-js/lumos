94fa84d68c142f31cdd5b91871227601866fc65d
wangximing <wangximing@cryptape.com>
2021-11-02 10:29:11 +0800

    chore(CI): fix ckb indexer path

--- devtools/azure/start-ckb-indexer.sh
+++ devtools/azure/start-ckb-indexer.sh
@@ -3,7 +3,8 @@
 nohup yarn workspace @ckb-lumos/testkit start &
 sleep 5
 pwd
-nohup ../../ckb-indexer -c http://127.0.0.1:8118/rpc -l 127.0.0.1:8120 -s indexer-store-tmp &
+ls
+nohup ./ckb-indexer -c http://127.0.0.1:8118/rpc -l 127.0.0.1:8120 -s indexer-store-tmp &
 sleep 5
 echo '{
     "id": 2,
