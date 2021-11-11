5ff6fe958184f8613430aa13527020a079366f19
homura <homura.dev@gmail.com>
2021-11-11 18:19:52 +0800

    refactor: types into modules

---
+++ packages/base/lib/helpers.d.ts
@@ -0,0 +1,7 @@
+import { Cell } from "./api";
+import { QueryOptions } from "./indexer";
+
+export declare function isCellMatchQueryOptions(
+  cell: Cell,
+  queryOptions: QueryOptions
+): boolean;
---
+++ packages/base/lib/logger.d.ts
@@ -0,0 +1,4 @@
+export type LogLevel = "warn" | "error" | string;
+// Indexer
+export type Logger = (level: LogLevel, message: string) => void;
+export type Log = (message: string) => void;
