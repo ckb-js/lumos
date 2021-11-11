5ce39fc5cad3a74dd089965d926363e77afff6a6
homura <homura.dev@gmail.com>
2021-11-11 21:14:06 +0800

    fix: cannot find deprecated

--- packages/base/lib/logger.d.ts
+++ packages/base/lib/logger.d.ts
@@ -2,3 +2,4 @@ export type LogLevel = "warn" | "error" | string;
 // Indexer
 export type Logger = (level: LogLevel, message: string) => void;
 export type Log = (message: string) => void;
+export function deprecated(message: string): void;
