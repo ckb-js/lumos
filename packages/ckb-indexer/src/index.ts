f2b54e90010069d9a6f526661c4156c1139def28
wangximing <wangximing@cryptape.com>
2021-11-25 20:26:48 +0800

    feat(ckb-indexer): add ckb-indexer rpc module

--- packages/ckb-indexer/src/index.ts
+++ packages/ckb-indexer/src/index.ts
@@ -1,6 +1,7 @@
 import { CKBCellCollector } from "./collector";
 import { CkbIndexer } from "./indexer";
 import { CKBIndexerTransactionCollector } from "./transaction_collector";
+import { RPC as CKBRPC } from "./rpc";
 
 /** CkbIndexer.collector will not get cell with block_hash by default, please use OtherQueryOptions.withBlockHash and OtherQueryOptions.CKBRpcUrl to get block_hash if you need. */
 export const Indexer = CkbIndexer;
@@ -8,3 +9,4 @@ export const Indexer = CkbIndexer;
 /** CellCollector will not get cell with block_hash by default, please use OtherQueryOptions.withBlockHash and OtherQueryOptions.CKBRpcUrl to get block_hash if you need. */
 export const CellCollector = CKBCellCollector;
 export const TransactionCollector = CKBIndexerTransactionCollector;
+export const RPC = CKBRPC;
---
+++ packages/ckb-indexer/src/rpc.ts
@@ -0,0 +1,70 @@
+import { RPC as ToolkitRPC } from "ckb-js-toolkit";
+import { HexString, Tip } from "@ckb-lumos/base";
+import {
+  GetCellsResults,
+  GetTransactionsResults,
+  Order,
+  SearchKey,
+} from "./indexer";
+
+const handler = {
+  get: (target: any, method: string) => {
+    return async (...params: any) => {
+      const result = await target.rpc[method](...params);
+      return result;
+    };
+  },
+};
+class RpcProxy {
+  private rpc: ToolkitRPC;
+  constructor(uri: string) {
+    this.rpc = new ToolkitRPC(uri);
+  }
+
+  getProxy() {
+    return new Proxy(this, handler);
+  }
+}
+
+export class RPC {
+  private rpcProxy: any;
+
+  /**
+   *
+   * @param uri  indexer uri
+   */
+  constructor(uri: string) {
+    this.rpcProxy = new RpcProxy(uri).getProxy();
+  }
+
+  async get_tip(): Promise<Tip> {
+    return this.rpcProxy.get_tip();
+  }
+  async get_cells(
+    searchKey: SearchKey,
+    order: Order,
+    limit: HexString,
+    after_cursor?: string
+  ): Promise<GetCellsResults> {
+    return this.rpcProxy.get_cells(searchKey, order, limit, after_cursor);
+  }
+  async get_transactions(
+    searchKey: SearchKey,
+    order: Order,
+    limit: HexString,
+    after_cursor?: string
+  ): Promise<GetTransactionsResults> {
+    return this.rpcProxy.get_transactions(
+      searchKey,
+      order,
+      limit,
+      after_cursor
+    );
+  }
+  async get_cells_capacity(searchKey: SearchKey) {
+    return this.rpcProxy.get_cells_capacity(searchKey);
+  }
+  async get_indexer_info(): Promise<string> {
+    return this.rpcProxy.get_indexer_info();
+  }
+}
