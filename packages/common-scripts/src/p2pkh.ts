d1e51dbdb1838efc75e50ec0aa071248ba8f56f6
beiqian <liubq97@gmail.com>
2022-02-10 11:44:18 +0800

    docs: p2pkh docs

--- packages/common-scripts/src/p2pkh.ts
+++ packages/common-scripts/src/p2pkh.ts
@@ -29,7 +29,7 @@ function groupInputs(inputs: Cell[], locks: Script[]): Map<string, number[]> {
   return groups;
 }
 
-export function calcRawTxHash(tx: TransactionSkeletonType): Reader {
+function calcRawTxHash(tx: TransactionSkeletonType): Reader {
   return utils.ckbHash(
     core.SerializeRawTransaction(
       toolkit.normalizers.NormalizeRawTransaction(
@@ -50,6 +50,17 @@ type Group = {
   message: Hash;
 };
 
+/**
+ * Return an array of messages as well as their corresponding position indexes and locks for signing a P2PKH transaction.
+ * For more details, please see: 
+ * https://github.com/nervosnetwork/ckb-system-scripts/wiki/How-to-sign-transaction
+ * 
+ * @param tx TxSkeleton with all input cells' witnessArgs.lock filled with 0.
+ * @param locks Locks you want to sign, e.g. you don't need to sign ACP cells.
+ * @param hasher Message hasher, defaults to CKB blake2b hasher. Check 
+ * https://github.com/nervosnetwork/ckb-system-scripts/blob/e975e8b7d5231fdb1c537b830dd934b305492417/c/secp256k1_blake160_sighash_all.c#L22-L28 for more.
+ * @returns An array of Group containing: lock of the input cell you need to sign, message for signing, witness index of this message (first index of the input cell with this lock).
+ */
 export function createP2PKHMessageGroup(
   tx: TransactionSkeletonType,
   locks: Script[],
