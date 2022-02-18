d14c08c614efa0b382d151d8f44b7cb7c9f81dbc
beiqian <liubq97@gmail.com>
2022-02-18 16:01:46 +0800

    feat: cardano lock example

--- examples/cardano-lock-namiwallet/src/App.tsx
+++ examples/cardano-lock-namiwallet/src/App.tsx
@@ -1,40 +1,88 @@
 import { Address } from "@emurgo/cardano-serialization-lib-browser";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { CIP30FullAPI, detectCardano } from "./lib";
-
-function useCardano() {
-  const [api, setAPI] = useState<CIP30FullAPI>();
-  const [cardanoAddress, setAddress] = useState<Address>();
-
-  useEffect(() => {
-    (async () => {
-      const cardano = await detectCardano();
-      const api = await cardano.nami.enable();
-
-      const [address] = await api.getUsedAddresses();
-
-      setAPI(api);
-      setAddress(Address.from_bytes(Buffer.from(address, "hex")));
-    })();
-  }, []);
-
-  if (!api || !cardanoAddress) return null;
-  return { api, address: cardanoAddress };
-}
+import { capacityOf, CIP30FullAPI, CONFIG, detectCardano, transfer } from "./lib";
+import { helpers, Script } from "@ckb-lumos/lumos";
 
 function App() {
-  const api = useCardano();
+  const [api, setAPI] = useState<CIP30FullAPI>();
+  const [cardanoAddr, setCardanoAddr] = useState("");
 
-  if (!api) {
-    return <div>Waiting for connect to NamiWallet</div>;
+  const [ckbAddr, setCkbAddr] = useState("");
+  const [lock, setLock] = useState<Script>();
+  const [balance, setBalance] = useState("-");
+
+  const [transferAddr, setTransferAddress] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
+
+  const [isSendingTx, setIsSendingTx] = useState(false);
+  const [txHash, setTxHash] = useState("");
+
+  async function connectToNami() {
+    const cardano = await detectCardano();
+    cardano.nami
+      .enable()
+      .then((api) => {
+        setAPI(api);
+        return api.getUsedAddresses();
+      })
+      .then(([address]) => {
+        setCardanoAddr(address);
+
+        const cardanoLock: Script = {
+          code_hash: CONFIG.SCRIPTS.CARDANO_LOCK.CODE_HASH,
+          hash_type: CONFIG.SCRIPTS.CARDANO_LOCK.HASH_TYPE,
+          args: address,
+        };
+        const ckbAddr = helpers.generateAddress(cardanoLock);
+        setLock(cardanoLock);
+        setCkbAddr(ckbAddr);
+
+        return capacityOf(ckbAddr);
+      })
+      .then((capacity) => setBalance(capacity.div(10 ** 8).toString() + " CKB"));
   }
+
+  function onTransfer() {
+    if (isSendingTx) return;
+    setIsSendingTx(true);
+
+    transfer({ amount: transferAmount, from: ckbAddr, to: transferAddr, api: api!, cardanoAddr: cardanoAddr })
+      .then(setTxHash)
+      // .catch((e) => alert(e.message || JSON.stringify(e)))
+      .finally(() => setIsSendingTx(false));
+  }
+
+  if (!cardanoAddr) return <button onClick={connectToNami}>Connect to Nami</button>;
 
   return (
     <div>
       <ul>
-        <li>Cardano Address: {api.address.to_bech32()}</li>
+        <li>Cardano Address: {Address.from_bytes(Buffer.from(cardanoAddr, "hex")).to_bech32()}</li>
+
+        <li>Nervos Address: {ckbAddr}</li>
+        <li>
+          Current lock script:
+          <pre>{JSON.stringify(lock, null, 2)}</pre>
+        </li>
+
+        <li>Balance: {balance}</li>
       </ul>
+
+      <div>
+        <h2>Transfer to</h2>
+        <label htmlFor="address">Address</label>&nbsp;
+        <input id="address" type="text" onChange={(e) => setTransferAddress(e.target.value)} placeholder="ckt1..." />
+        <br />
+        <label htmlFor="amount">Amount</label>
+        &nbsp;
+        <input id="amount" type="text" onChange={(e) => setTransferAmount(e.target.value)} placeholder="shannon" />
+        <br />
+        <button onClick={onTransfer} disabled={isSendingTx}>
+          Transfer
+        </button>
+        <p>Tx Hash: {txHash}</p>
+      </div>
     </div>
   );
 }
--- examples/cardano-lock-namiwallet/src/lib.ts
+++ examples/cardano-lock-namiwallet/src/lib.ts
@@ -1,4 +1,25 @@
-// https://cips.cardano.org/cips/cip30/
+import { BI, Cell, config, core, helpers, Indexer, RPC, toolkit, utils } from "@ckb-lumos/lumos";
+
+export const CONFIG = config.createConfig({
+  PREFIX: "ckt",
+  SCRIPTS: {
+    ...config.predefined.AGGRON4.SCRIPTS,
+    CARDANO_LOCK: {
+      CODE_HASH: "0x3625f5ccecdbb8edff6890db2225b0218d753b7932e144a41b0a77b1111c921b",
+      HASH_TYPE: "type",
+      TX_HASH: "0xadf72b5a58b18e3763ab9e7769e16ffcb222da07a2cae2b407a6ffc47a2d39ff",
+      INDEX: "0x0",
+      DEP_TYPE: "code",
+    },
+  },
+});
+
+config.initializeConfig(CONFIG);
+
+const CKB_RPC_URL = "https://testnet.ckb.dev/rpc";
+const CKB_INDEXER_URL = "https://testnet.ckb.dev/indexer";
+const rpc = new RPC(CKB_RPC_URL);
+const indexer = new Indexer(CKB_INDEXER_URL, CKB_RPC_URL);
 
 export interface Cardano {
   nami: {
@@ -7,7 +28,7 @@ export interface Cardano {
 }
 export interface CIP30FullAPI {
   getUsedAddresses: () => Promise<string[]>;
-  signData: (address: string, message: string) => Promise<string>;
+  signData: (address: string, message: string) => Promise<{ signature: string; key: string }>;
 }
 
 declare global {
@@ -25,4 +46,122 @@ export async function detectCardano(): Promise<Cardano> {
     if (window.cardano) return window.cardano;
     await new Promise((resolve) => setTimeout(resolve, 100));
   }
+}
+
+interface Options {
+  from: string;
+  to: string;
+  amount: string;
+  api: CIP30FullAPI;
+  cardanoAddr: string;
+}
+
+export async function transfer(options: Options): Promise<string> {
+  let tx = helpers.TransactionSkeleton({});
+  const fromScript = helpers.parseAddress(options.from);
+  const toScript = helpers.parseAddress(options.to);
+
+  // additional 0.001 ckb for tx fee
+  // the tx fee could calculated by tx size
+  // this is just a simple example
+  const neededCapacity = BI.from(options.amount).add("100000");
+  let collectedSum = BI.from(0);
+  const collectedCells: Cell[] = [];
+  const collector = indexer.collector({ lock: fromScript, type: "empty" });
+  for await (const cell of collector.collect()) {
+    collectedSum = collectedSum.add(cell.cell_output.capacity);
+    collectedCells.push(cell);
+    if (BI.from(collectedSum).gte(neededCapacity)) break;
+  }
+
+  if (collectedSum.lt(neededCapacity)) {
+    throw new Error(`Not enough CKB, expected: ${neededCapacity}, actual: ${collectedSum} `);
+  }
+
+  const transferOutput: Cell = {
+    cell_output: {
+      capacity: BI.from(options.amount).toHexString(),
+      lock: toScript,
+    },
+    data: "0x",
+  };
+
+  const changeOutput: Cell = {
+    cell_output: {
+      capacity: collectedSum.sub(neededCapacity).toHexString(),
+      lock: fromScript,
+    },
+    data: "0x",
+  };
+
+  tx = tx.update("inputs", (inputs) => inputs.push(...collectedCells));
+  tx = tx.update("outputs", (outputs) => outputs.push(transferOutput, changeOutput));
+  tx = tx.update("cellDeps", (cellDeps) =>
+    cellDeps.push({
+      out_point: {
+        tx_hash: CONFIG.SCRIPTS.CARDANO_LOCK.TX_HASH,
+        index: CONFIG.SCRIPTS.CARDANO_LOCK.INDEX,
+      },
+      dep_type: CONFIG.SCRIPTS.CARDANO_LOCK.DEP_TYPE,
+    })
+  );
+
+  const payload = "00".repeat(32);
+  let tmpWitness = (await options.api.signData(options.cardanoAddr, payload)).signature;
+  tmpWitness = new toolkit.Reader(
+    core.SerializeWitnessArgs({
+      lock: new toolkit.Reader("0x" + tmpWitness),
+    })
+  ).serializeJson();
+
+  const hasher = new utils.CKBHasher();
+  const rawTxHash = utils.ckbHash(
+    core.SerializeRawTransaction(toolkit.normalizers.NormalizeRawTransaction(helpers.createTransactionFromSkeleton(tx)))
+  );
+  hasher.update(rawTxHash);
+
+  const SECP_SIGNATURE_PLACEHOLDER = new toolkit.Reader("0x" + "00".repeat(tmpWitness.length - 20));
+  const newWitnessArgs = { lock: SECP_SIGNATURE_PLACEHOLDER };
+  const witness = core.SerializeWitnessArgs(newWitnessArgs);
+
+  hashWitness(hasher, witness);
+  const messageForSigning = hasher.digestHex();
+
+  const signedRes = await options.api.signData(options.cardanoAddr, messageForSigning.slice(2));
+  const signedMessage = signedRes.signature;
+
+  const signedWitness = new toolkit.Reader(
+    core.SerializeWitnessArgs({
+      lock: new toolkit.Reader("0x" + signedMessage),
+    })
+  ).serializeJson();
+
+  tx = tx.update("witnesses", (witnesses) => witnesses.push(signedWitness));
+
+  const signedTx = helpers.createTransactionFromSkeleton(tx);
+  const txHash = await rpc.send_transaction(signedTx, "passthrough");
+
+  return txHash;
+}
+
+function hashWitness(hasher: utils.CKBHasher, witness: ArrayBuffer): void {
+  const lengthBuffer = new ArrayBuffer(8);
+  const view = new DataView(lengthBuffer);
+  view.setBigUint64(0, BigInt(new toolkit.Reader(witness).length()), true);
+
+  hasher.update(lengthBuffer);
+  hasher.update(witness);
+}
+
+export async function capacityOf(address: string): Promise<BI> {
+  const collector = indexer.collector({
+    lock: helpers.parseAddress(address),
+  });
+
+  let balance = BI.from(0);
+  for await (const cell of collector.collect()) {
+    balance = balance.add(cell.cell_output.capacity);
+  }
+
+  return balance;
 }
