5f53a4936cb11642413a0e9be9b50957813977de
wangximing <sighwang@outlook.com>
2022-03-08 23:04:08 +0800

    test(ckb-indexer): add getResolvedTransactionRequestPayload and fetchResolvedTransaction unit test

--- packages/ckb-indexer/tests/transaction_collector.unit.test.ts
+++ packages/ckb-indexer/tests/transaction_collector.unit.test.ts
@@ -1,117 +1,142 @@
 import test from "ava";
 import { Indexer, TransactionCollector } from "../src";
-import { SearchKey } from "../src/type";
-import sinon, { SinonStub } from "sinon";
+import { JsonRprRequestBody } from "../src/type";
+import sinon, { SinonSpy } from "sinon";
 import * as services from "../src/services";
+
 import {
-  batchRequestIoTypeInput,
-  getTransactionFromIndexerResult,
-  ioTypeInputResult,
+  indexerTransactionListThatHaveOneIoTypeInput,
+  indexerTransactionListThatHaveTwoIoTypeInput,
+  indexerTransactionListThatHaveZeroIoTypeInput,
+  unresolvedTransactionList,
   queryOption,
-  multipleInputCellTx,
-  multipleInputCellBatchRequestResult,
-  batchForAll,
-  batchForInput,
-  batchForInputResult,
-  multipleInputQuery,
-  batchRequest,
-} from "./transaction_collector_unit_test_case";
+} from "./transaction_collector_unit_testcase";
 
 const nodeUri = "http://127.0.0.1:8118/rpc";
 const indexUri = "http://127.0.0.1:8120";
 const indexer = new Indexer(indexUri, nodeUri);
-let getTransactionsStub: SinonStub;
-let requestBatchStub: SinonStub;
-
-test("input cell can be found in transaction detail", async (t) => {
-  getTransactionsStub = sinon.stub(indexer, "getTransactions");
-  requestBatchStub = sinon.stub(services, "requestBatch");
-  const searchKey1: SearchKey = {
-    script: {
-      code_hash:
-        "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-      hash_type: "type",
-      args: "0xbde8b19b4505dd1d1310223edecea20adc4e240e",
-    },
-    script_type: "lock",
-    filter: {},
-  };
-  const SearchFilter1 = { sizeLimit: undefined, order: undefined };
-  getTransactionsStub
-    .withArgs(searchKey1, SearchFilter1)
-    .returns(getTransactionFromIndexerResult);
-  const searchKey2: SearchKey = {
-    script: {
-      code_hash:
-        "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-      hash_type: "type",
-      args: "0xbde8b19b4505dd1d1310223edecea20adc4e240e",
-    },
-    script_type: "lock",
-    filter: {},
-  };
-  const searchKeyFilter2 = {
-    sizeLimit: undefined,
-    order: undefined,
-    lastCursor:
-      "0x809bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce801bde8b19b4505dd1d1310223edecea20adc4e240e000000000021420d000000010000000000",
-  };
-  getTransactionsStub.withArgs(searchKey2, searchKeyFilter2).returns(
-    Promise.resolve({
-      lastCursor:
-        "0x809bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce801bde8b19b4505dd1d1310223edecea20adc4e240e000000000021420d000000010000000000",
-      objects: [],
-    })
-  );
-  requestBatchStub.withArgs(...batchRequest.args).returns(batchRequest.result);
-  requestBatchStub
-    .withArgs(nodeUri, batchRequestIoTypeInput)
-    .returns(ioTypeInputResult);
-  const cellCollector = new TransactionCollector(indexer, queryOption, nodeUri);
-  const count = await cellCollector.count();
-  t.is(count, 1);
-  getTransactionsStub.reset();
-  requestBatchStub.reset();
-  getTransactionsStub.restore();
-  requestBatchStub.restore();
+let requestBatchSpy: SinonSpy;
+test.before(() => {
+  requestBatchSpy = sinon.spy(services, "requestBatch");
 });
+test.afterEach(() => {
+  requestBatchSpy.resetHistory();
+});
+test.serial(
+  "getResolvedTransactionRequestPayload# should return empty list if no indexerTransaction ioType is input",
+  (t) => {
+    const transactionCollector = new TransactionCollector(
+      indexer,
+      queryOption,
+      nodeUri
+    );
+    const resolvedTransactionList = transactionCollector.getResolvedTransactionRequestPayload(
+      unresolvedTransactionList,
+      indexerTransactionListThatHaveZeroIoTypeInput
+    );
+    const expectedResult: JsonRprRequestBody[] = [];
+    t.deepEqual(resolvedTransactionList, expectedResult);
+  }
+);
 
-test("should add inputCell to all transaction which txHash and ioType is same as query", async (t) => {
-  const searchKey: SearchKey = {
-    script: {
-      code_hash:
-        "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
-      hash_type: "type",
-      args: "0xdfc8ad219178a307ff117ee4e2fe760cd18410a7",
-    },
-    script_type: "lock",
-    filter: {},
-  };
-  const SearchFilter = { sizeLimit: undefined, order: undefined };
-  getTransactionsStub
-    .withArgs(searchKey, SearchFilter)
-    .returns(multipleInputCellTx);
-  requestBatchStub
-    .withArgs(nodeUri, batchForAll)
-    .returns(multipleInputCellBatchRequestResult);
-  requestBatchStub
-    .withArgs(nodeUri, batchForInput)
-    .returns(batchForInputResult);
+test.serial(
+  "getResolvedTransactionRequestPayload# should return correct requestPayload if one indexerTransaction ioType is input",
+  (t) => {
+    const transactionCollector = new TransactionCollector(
+      indexer,
+      queryOption,
+      nodeUri
+    );
+    const resolvedTransactionList = transactionCollector.getResolvedTransactionRequestPayload(
+      unresolvedTransactionList,
+      indexerTransactionListThatHaveOneIoTypeInput
+    );
+    const expectedResult: JsonRprRequestBody[] = [
+      {
+        id: 0,
+        jsonrpc: "2.0",
+        method: "get_transaction",
+        params: [
+          "0x992208eab19d0f8ce5a2fc10579d8d614d265aa12851ea140ec717f2f41b925f",
+        ],
+      },
+    ];
+    t.deepEqual(resolvedTransactionList, expectedResult);
+  }
+);
+
+test.serial(
+  "getResolvedTransactionRequestPayload# should return correct requestPayload if two indexerTransaction ioType is input",
+  (t) => {
+    const transactionCollector = new TransactionCollector(
+      indexer,
+      queryOption,
+      nodeUri
+    );
+    const resolvedTransactionList = transactionCollector.getResolvedTransactionRequestPayload(
+      unresolvedTransactionList,
+      indexerTransactionListThatHaveTwoIoTypeInput
+    );
+    const expectedResult: JsonRprRequestBody[] = [
+      {
+        id: 0,
+        jsonrpc: "2.0",
+        method: "get_transaction",
+        params: [
+          "0x992208eab19d0f8ce5a2fc10579d8d614d265aa12851ea140ec717f2f41b925f",
+        ],
+      },
+      {
+        id: 1,
+        jsonrpc: "2.0",
+        method: "get_transaction",
+        params: [
+          "0x805168dafc0c10ae31de2580541db0f5ee8ff53afb55e39a5e2eeb60f878553f",
+        ],
+      },
+    ];
+    t.deepEqual(resolvedTransactionList, expectedResult);
+  }
+);
+
+test.serial("fetchResolvedTransaction#", async (t) => {
   const transactionCollector = new TransactionCollector(
     indexer,
-    multipleInputQuery,
+    queryOption,
     nodeUri
   );
-  const result = await transactionCollector.getTransactions();
-  result.objects.forEach((tx) => {
-    t.is(
-      tx.transaction.hash,
-      "0x87c3586cc91ec7a1a97407456dba0adb34c5781c6031cb71bfb175939af4e6c6"
-    );
-  });
-  t.is(result.objects.length, 5);
-  getTransactionsStub.reset();
-  requestBatchStub.reset();
-  getTransactionsStub.restore();
-  requestBatchStub.restore();
+  const emptyPayload: JsonRprRequestBody[] = [];
+  const emptyResolvedTransaction = await transactionCollector.fetchResolvedTransaction(
+    emptyPayload
+  );
+  t.is(
+    requestBatchSpy.called,
+    false,
+    "empty request payload should not call batchRequest"
+  );
+  t.deepEqual(
+    emptyResolvedTransaction,
+    [],
+    "should return empty result if request payload is empty"
+  );
+
+  const payloadWithData: JsonRprRequestBody[] = [
+    {
+      id: 0,
+      jsonrpc: "2.0",
+      method: "get_transaction",
+      params: [
+        "0x992208eab19d0f8ce5a2fc10579d8d614d265aa12851ea140ec717f2f41b925f",
+      ],
+    },
+  ];
+  const resolvedTransactionList = await transactionCollector.fetchResolvedTransaction(
+    payloadWithData
+  );
+  t.is(
+    requestBatchSpy.called,
+    true,
+    "should call batchRequest if request payload not empty"
+  );
+  t.is(resolvedTransactionList.length, 1, "should return correct length data");
 });
