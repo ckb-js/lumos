881904054d37f44aeabe1397262a60d2fd54973e
beiqian <liubq97@gmail.com>
2022-03-11 15:16:41 +0800

    fix: export deploy generator

--- packages/common-scripts/src/index.ts
+++ packages/common-scripts/src/index.ts
@@ -8,6 +8,12 @@ import sudt from "./sudt";
 import anyoneCanPay from "./anyone_can_pay";
 import { parseFromInfo } from "./from_info";
 import { createP2PKHMessageGroup } from "./p2pkh";
+import {
+  generateDeployWithDataTx,
+  generateDeployWithTypeIdTx,
+  generateUpgradeTypeIdDataTx,
+  compareScriptBinaryWithOnChainData,
+} from "./deploy";
 
 export {
   secp256k1Blake160,
@@ -23,6 +29,10 @@ export {
   LockScriptInfo,
   parseFromInfo,
   createP2PKHMessageGroup,
+  generateDeployWithDataTx,
+  generateDeployWithTypeIdTx,
+  generateUpgradeTypeIdDataTx,
+  compareScriptBinaryWithOnChainData,
 };
 
 export default {
