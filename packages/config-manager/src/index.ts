7f2a08df02df8e1c5d042c804ed751bfa1efcbc1
liubq7 <liubq97@gmail.com>
2022-03-14 18:43:14 +0800

    style: prettier

--- packages/config-manager/src/index.ts
+++ packages/config-manager/src/index.ts
@@ -1,5 +1,10 @@
 export * from "./types";
-export { initializeConfig, getConfig, validateConfig, CKB2019,
-    CKB2021, } from "./manager";
+export {
+  initializeConfig,
+  getConfig,
+  validateConfig,
+  CKB2019,
+  CKB2021,
+} from "./manager";
 export * as helpers from "./helpers";
 export { predefined, createConfig } from "./predefined";
