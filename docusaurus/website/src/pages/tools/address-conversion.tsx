735edfaed8b79ad9d9e317953b50f039cd3e327d
liubq7 <liubq97@gmail.com>
2022-03-14 16:28:35 +0800

    feat: move address conversion tools for lumos website from branch ckb2021

---
+++ docusaurus/website/src/pages/tools/address-conversion.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Address } from "../../components/Address";
+import { Sidebar } from "../../components/Sidebar";
+import Layout from "@theme/Layout";
+
+export default function Tools() {
+    return (
+        <Layout title="Tools" description="">
+            <div style={{ display: "flex" }}>
+                <Sidebar></Sidebar>
+                <Address></Address>
+            </div>
+        </Layout>
+    );
+}
