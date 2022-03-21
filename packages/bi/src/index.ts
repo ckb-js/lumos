6aac6e3592cde4177f12cf351f101a680be7aaf3
liubq7 <liubq97@gmail.com>
2022-03-21 17:08:33 +0800

    fix: bi eslint

--- packages/bi/src/index.ts
+++ packages/bi/src/index.ts
@@ -1,7 +1,7 @@
 import JSBI from "jsbi";
 export type BIish = number | string | bigint | BI;
 
-export function isBIish(value: any): value is BIish {
+export function isBIish(value: unknown): value is BIish {
   return (
     value !== null &&
     ((typeof value === "number" && value % 1 === 0) ||
@@ -152,7 +152,7 @@ export class BI {
     }
   }
 
-  static from(value: any): BI {
+  static from(value: unknown): BI {
     if (value instanceof BI) {
       return value;
     } else if (isBIish(value)) {
@@ -164,9 +164,14 @@ export class BI {
     }
   }
 
-  static isBI(value: any): value is BI {
-    return !!(value && value._isBI);
+  static isBI(value: unknown): value is BI {
+    return isBILike(value) && !!value._isBI;
   }
+}
+
+function isBILike(value: unknown): value is Record<string, unknown> {
+  if (!value) return false;
+  return typeof value === "object";
 }
 
 function toBI(value: JSBI): BI {
