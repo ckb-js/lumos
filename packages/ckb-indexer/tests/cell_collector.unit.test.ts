da326998ff915fbaae6fa6737b93ab6a3b910169
wangximing <sighwang@outlook.com>
2022-03-29 09:50:11 +0800

    test(ckb-indexer): add some query option validate test

--- packages/ckb-indexer/tests/cell_collector.unit.test.ts
+++ packages/ckb-indexer/tests/cell_collector.unit.test.ts
@@ -1,11 +1,22 @@
 import test from "ava";
 import { Indexer, CellCollector } from "../src";
-import { HexadecimalRange, Script } from "@ckb-lumos/base";
+import { HexadecimalRange, Script, utils } from "@ckb-lumos/base";
+import sinon, { SinonSpy } from "sinon";
+import { validators } from "@ckb-lumos/toolkit";
 
 const nodeUri = "http://127.0.0.1:8118/rpc";
 const indexUri = "http://127.0.0.1:8120";
 const indexer = new Indexer(indexUri, nodeUri);
 
+let validateScriptSpy: SinonSpy;
+let utilsSpy: SinonSpy;
+test.before(() => {
+  validateScriptSpy = sinon.spy(validators, "ValidateScript");
+  utilsSpy = sinon.spy(utils, "assertHexadecimal");
+});
+test.afterEach(() => {
+  validateScriptSpy.resetHistory();
+});
 const lockScript: Script = {
   code_hash:
     "0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8",
@@ -52,4 +63,74 @@ test("convertParams# should match outputDataRange if data and outputData both de
     new CellCollector(indexer, notMatchQuery);
   });
   t.is(error.message, "data length not match outputDataLenRange");
+});
+
+test("validateQueryOption#should throw error if lock and type not provided", (t) => {
+  t.throws(
+    () => {
+      new CellCollector(indexer, {});
+    },
+    undefined,
+    "throw error if lock and query both not provided"
+  );
+
+  t.throws(
+    () => {
+      new CellCollector(indexer, {
+        type: "empty",
+      });
+    },
+    undefined,
+    "throw error if lock is not provided and type is empty"
+  );
+});
+
+test("validateQueryOption#validate lock if lock is script", (t) => {
+  const query = {
+    lock: lockScript,
+  };
+  new CellCollector(indexer, query);
+  t.is(validateScriptSpy.calledWith(query.lock), true);
+});
+
+test("validateQueryOption#validate lock.script if lock is ScriptWrapper", (t) => {
+  const query = {
+    lock: { script: lockScript, argsLen: 20 },
+  };
+  new CellCollector(indexer, query);
+  t.is(validateScriptSpy.calledWith(query.lock.script), true);
+});
+
+test("validateQueryOption#validate type if type is script", (t) => {
+  const query = {
+    type: lockScript,
+  };
+  new CellCollector(indexer, query);
+  t.is(validateScriptSpy.calledWith(query.type), true);
+});
+
+test("validateQueryOption#validate type.script if type is ScriptWrapper", (t) => {
+  const query = {
+    type: { script: lockScript, argsLen: 20 },
+  };
+  new CellCollector(indexer, query);
+  t.is(validateScriptSpy.calledWith(query.type.script), true);
+});
+
+test("validateQueryOption#validate fromBlock", (t) => {
+  const query = {
+    lock: lockScript,
+    fromBlock: "0x1",
+  };
+  new CellCollector(indexer, query);
+  t.is(utilsSpy.calledWith("fromBlock", "0x1"), true);
+});
+
+test("validateQueryOption#validate toBlock", (t) => {
+  const query = {
+    lock: lockScript,
+    toBlock: "0x3",
+  };
+  new CellCollector(indexer, query);
+  t.is(utilsSpy.calledWith("toBlock", "0x3"), true);
 });
