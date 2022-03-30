12a3141dc6e7b0d919798d440acc8a7e4eac1b24
Beiqian Liu <liubq97@gmail.com>
2022-03-30 16:24:48 +0800

    feat(website): contract deployment tool for lumos website (#306)
    * feat: move address conversion tools for lumos website from branch ckb2021
    
    * fix: add ckb2019 and ckb2021 func
    
    * style: prettier
    
    * refactor: remove ckb2019 function in config manager
    
    * fix: convert address without shortid
    
    * refactor: update website lumos dep version
    
    * fix: remove ckb2019 api
    
    * refactor: move address conversion tool to a single folder
    
    * feat: contract deployment tool for website
    
    * refactor: change the way of import
    
    * feat: disable button while sending tx
    
    * fix: typo
    
    * fix: deploy contract bytesToHex
    
    * fix: vercel url

--- docusaurus/website/src/components/Sidebar.tsx
+++ docusaurus/website/src/components/Sidebar.tsx
@@ -3,18 +3,22 @@ import { Menu } from "antd";
 import Link from "@docusaurus/Link";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
-export const Sidebar = () => {
+export const Sidebar = (props) => {
     const keyAddressConversion = useBaseUrl("/tools/address-conversion");
+    const contractDeployment = useBaseUrl("/tools/contract-deployment");
 
     return (
-        <Menu
-            style={{ width: 256 }}
-            defaultSelectedKeys={[keyAddressConversion]}
-            mode="inline"
-        >
-            <Menu.Item key={keyAddressConversion}>
-                <Link to="/tools/address-conversion">Address Conversion</Link>
-            </Menu.Item>
-        </Menu>
+      <Menu
+        style={{ width: 256 }}
+        defaultSelectedKeys={[useBaseUrl(props.url)]}
+        mode="inline"
+      >
+          <Menu.Item key={keyAddressConversion}>
+              <Link to="/tools/address-conversion">Address Conversion</Link>
+          </Menu.Item>
+          <Menu.Item key={contractDeployment}>
+              <Link to="/tools/contract-deployment">Contract Deployment</Link>
+          </Menu.Item>
+      </Menu>
     );
 };
