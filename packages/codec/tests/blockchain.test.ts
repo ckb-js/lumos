3a3d2328c96fbd9fa953c33152964fa0737c858e
homura <homura.dev@gmail.com>
2022-05-06 19:17:43 +0800

    fix(codec): incorrect witness table conversion

--- packages/codec/tests/blockchain.test.ts
+++ packages/codec/tests/blockchain.test.ts
@@ -1,7 +1,38 @@
 import test from "ava";
-import { BytesOpt } from "../src/blockchain";
-import { bytify } from "../src/bytes";
+import { BytesOpt, WitnessArgs } from "../src/blockchain";
+import { bytify, hexify } from "../src/bytes";
 import { byteVecOf, table } from "../src/molecule";
+import { randomBytes } from "crypto";
+
+const SECP256K1_SIGNATURE_LENGTH = 65;
+
+test("secp256k1 witness args", (t) => {
+  const unsigned = WitnessArgs.pack({
+    lock: hexify(Buffer.alloc(SECP256K1_SIGNATURE_LENGTH)),
+  });
+
+  t.deepEqual(
+    unsigned,
+    bytify(
+      "0x55000000100000005500000055000000410000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+    )
+  );
+
+  const signature = bytify(randomBytes(SECP256K1_SIGNATURE_LENGTH));
+  const signed = WitnessArgs.pack({ lock: hexify(signature) });
+
+  t.deepEqual(
+    signed.slice(0, -SECP256K1_SIGNATURE_LENGTH),
+    unsigned.slice(0, -SECP256K1_SIGNATURE_LENGTH),
+    "header of signed witness is not valid"
+  );
+
+  t.deepEqual(
+    signed.slice(-SECP256K1_SIGNATURE_LENGTH),
+    signature,
+    "signature is not valid"
+  );
+});
 
 test("a real world Omni Lock witness should work as expected", (t) => {
   const OmniLockWitnessLock = table(
@@ -26,7 +57,7 @@ test("a real world Omni Lock witness should work as expected", (t) => {
 
   const packedWitness = OmniLockWitness.pack({
     // secp256k1 signature in CKB is 65 bytes
-    lock: { signature: "0x" + "00".repeat(65) },
+    lock: { signature: "0x" + "00".repeat(SECP256K1_SIGNATURE_LENGTH) },
   });
   const omniLockWitnessPlaceholder = bytify(
     // the hex is calculated by the following code
