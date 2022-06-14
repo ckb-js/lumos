22de9d64f34b2e82b756e9a956dce8118896fd1f
zhangyouxin <zhangyouxingg@126.com>
2022-06-15 07:43:24 +0800

    feat: implement p2pkh

--- packages/omnilock/src/suite/p2pkh.ts
+++ packages/omnilock/src/suite/p2pkh.ts
@@ -1,7 +1,11 @@
+import { hexify } from "@ckb-lumos/codec/lib/bytes";
 import { TransactionSkeletonType } from "@ckb-lumos/helpers";
 import { ScriptConfig } from "@ckb-lumos/config-manager";
-import { AdjustedSkeleton, AuthByP2PKH, AuthPart } from "../types";
-import { unimplemented } from "../utils";
+import { createP2PKHMessageGroup } from "@ckb-lumos/common-scripts";
+import { AdjustedSkeleton, AuthByP2PKH, AuthPart, SigningHint } from "../types";
+import { OmnilockWitnessLock } from "../codecs/witnesses";
+import { LockArgsCodec } from "../codecs/args";
+import { Script } from "@ckb-lumos/base";
 
 export function isP2PKHHint(x: AuthPart): x is AuthByP2PKH {
   return (
@@ -15,7 +19,45 @@ export function p2pkh(
   txSkeleton: TransactionSkeletonType,
   options: { config: ScriptConfig; hints: AuthByP2PKH[] }
 ): AdjustedSkeleton {
-  console.log(txSkeleton);
-  console.log(options);
-  unimplemented();
+  const scripts: Script[] = options.hints.map((hint) => {
+    const { CODE_HASH, HASH_TYPE } = options.config;
+    return {
+      code_hash: CODE_HASH,
+      hash_type: HASH_TYPE,
+      args: hexify(
+        LockArgsCodec.pack({
+          authFlag: hint.authFlag,
+          authContent: hint.options.pubkeyHash,
+          omnilockArgs: {},
+          omnilockFlags: {},
+        })
+      ),
+    };
+  });
+  const witnessPlaceholder = hexify(
+    OmnilockWitnessLock.pack({ signature: `0x${"00".repeat(65)}` })
+  );
+  const groups = createP2PKHMessageGroup(txSkeleton, scripts);
+  let adjustedSkeon = txSkeleton;
+  groups.forEach((group) => {
+    if (!txSkeleton.witnesses.get(group.index)) {
+      // initilize witness with placeholder if not exist
+      adjustedSkeon = txSkeleton.update("witnesses", (witnesses) =>
+        witnesses.set(group.index, witnessPlaceholder)
+      );
+    }
+  });
+  const signingHints: SigningHint[] = groups.map((group) => {
+    return {
+      script: group.lock,
+      index: group.index,
+      witnessArgItem: witnessPlaceholder,
+      signatureOffset: 0,
+      signatureLength: 65,
+    };
+  });
+  return {
+    adjusted: adjustedSkeon,
+    signingHints,
+  };
 }
