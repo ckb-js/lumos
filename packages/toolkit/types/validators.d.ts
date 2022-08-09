7b7bcbe574e81dc44f3e5d561ede946425da6432
zhangyouxin <zhangyouxingg@126.com>
2022-08-09 10:42:10 +0800

    change call signature of validators

--- packages/toolkit/types/validators.d.ts
+++ packages/toolkit/types/validators.d.ts
@@ -1,91 +1,92 @@
+import type * as api from './api'
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateScript(script: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): script is api.Script;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateOutPoint(outPoint: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): outPoint is api.OutPoint;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateCellInput(cellInput: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): cellInput is api.Input;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateCellOutput(cellOutput: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): cellOutput is api.Output;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateCellDep(cellDep: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): cellDep is api.CellDep;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateRawTransaction(rawTransaction: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): rawTransaction is api.RawTransaction;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateTransaction(transaction: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): transaction is api.Transaction;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateRawHeader(rawHeader: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): rawHeader is Omit<api.Header, 'hash'|'nonce'>;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateHeader(header: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): header is api.Header;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateUncleBlock(uncleBlock: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): uncleBlock is api.UncleBlock;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateBlock(block: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): block is api.Block;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateCellbaseWitness(cellbaseWitness: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): cellbaseWitness is api.CellbaseWitness;
 /**
  * @deprecated please follow the [migration-guide]{@link https://lumos-website.vercel.app/migrations/migrate-to-v0.19}
  */
 export function ValidateWitnessArgs(witnessArgs: any, { nestedValidation, debugPath }?: {
   nestedValidation?: boolean;
   debugPath?: string;
-}): void;
+}): witnessArgs is api.WitnessArgs;
