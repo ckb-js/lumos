6fca235b30aa7abb65b34b4f78e22a7270c0da8f
zhangyouxin <zhangyouxingg@126.com>
2022-08-15 08:38:23 +0800

    fix base export

--- packages/base/index.d.ts
+++ packages/base/index.d.ts
@@ -1,16 +1,16 @@
-import * as utils from "./src/utils";
-import * as helpers from "./src/helpers";
-import * as since from "./src/since";
-import * as values from "./src/values";
-import * as logger from "./src/logger";
-import * as blockchain from "./src/blockchain";
+import * as utils from "./lib/utils";
+import * as helpers from "./lib/helpers";
+import * as since from "./lib/since";
+import * as values from "./lib/values";
+import * as logger from "./lib/logger";
+import * as blockchain from "./lib/blockchain";
 
-export * from "./src/primitive";
-export * from "./src/api";
-export * from "./src/indexer";
+export * from "./lib/primitive";
+export * from "./lib/api";
+export * from "./lib/indexer";
 
 export { utils, helpers, since, values, logger, blockchain };
-export { SinceValidationInfo } from "./src/since";
+export { SinceValidationInfo } from "./lib/since";
 
 export interface Tip {
   blockNumber: string;
