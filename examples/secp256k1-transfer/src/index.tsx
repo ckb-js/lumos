dc53a3d473e6e15a5cfc50ed40028252d179e01e
Iron Lu <IronLu233@users.noreply.github.com>
2022-08-18 17:51:37 +0800

    feat(examples): refactor secp256k1-transfer example phase2 modify index.ts

--- examples/secp256k1-transfer/src/index.tsx
+++ examples/secp256k1-transfer/src/index.tsx
@@ -1,19 +1,56 @@
-import React, { useEffect, useState } from "react";
+import "bulma/css/bulma.css";
+import React, { ChangeEvent, useEffect, useMemo, useState } from "react";
 import ReactDOM from "react-dom";
-import { Script } from "@ckb-lumos/lumos";
-import { capacityOf, generateAccountFromPrivateKey, transfer } from "./lib";
+import { nanoid } from "nanoid";
+import { BI, Script, helpers } from "@ckb-lumos/lumos";
+import { capacityOf, createTxSkeleton, generateAccountFromPrivateKey, transfer } from "./lib";
+import { BIish } from "@ckb-lumos/bi";
 
-const app = document.getElementById("root");
-ReactDOM.render(<App />, app);
+type TxTarget = {
+  amount: BIish;
+  address: string;
+  key: string;
+};
+
+const genScenarioTxTarget = () => ({ key: nanoid(), amount: 0, address: "" });
 
 export function App() {
   const [privKey, setPrivKey] = useState("");
   const [fromAddr, setFromAddr] = useState("");
   const [fromLock, setFromLock] = useState<Script>();
   const [balance, setBalance] = useState("0");
+  const [txHash, setTxHash] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [toAddr, setToAddr] = useState("");
-  const [amount, setAmount] = useState("");
+  const [txTargets, setTxTargets] = useState<TxTarget[]>([genScenarioTxTarget()]);
+  const [txSkeleton, setTxSkeleton] = useState<ReturnType<typeof helpers.TransactionSkeleton> | undefined>();
+  const setTargetByIndex = (index: number, field: "amount" | "address") => (e: ChangeEvent<HTMLInputElement>) => {
+    setErrorMessage("");
+    const newTargets = [...txTargets];
+    if (field === "amount") {
+      newTargets[index].amount = Number(e.target.value);
+    } else {
+      newTargets[index]["address"] = e.target.value;
+    }
+    setTxTargets(newTargets);
+  };
+
+  const insertTxTarget = () => {
+    setTxTargets((origin) => [...origin, genScenarioTxTarget()]);
+  };
+
+  const removeTxTarget = (index: number) => () => {
+    setTxTargets((origin) => origin.filter((_, i) => i !== index));
+  };
+
+  const txOptions = useMemo(
+    () => ({
+      from: fromAddr,
+      targets: txTargets.map((tx) => ({ to: tx.address, amount: BI.from(tx.amount) })),
+      privKey,
+    }),
+    [fromAddr, txTargets, privKey]
+  );
 
   useEffect(() => {
     const updateFromInfo = async () => {
@@ -24,50 +61,149 @@ export function App() {
       setBalance(capacity.toString());
     };
 
+    setErrorMessage("");
     if (privKey) {
-      updateFromInfo();
+      updateFromInfo().catch((e: Error) => {
+        setErrorMessage(e.toString());
+      });
     }
   }, [privKey]);
 
-  return (
-    <div>
-      <label htmlFor="private-key">Private Key: </label>&nbsp;
-      <input
-        id="private-key"
-        type="text"
-        onChange={(e) => setPrivKey(e.target.value)}
-      />
-      <ul>
-        <li>CKB Address: {fromAddr}</li>
-        <li>
-          Current lock script:
-          <pre>{JSON.stringify(fromLock, null, 2)}</pre>
-        </li>
+  useEffect(() => {
+    (async () => {
+      if (!txOptions.privKey || !txOptions.from) {
+        return;
+      }
+      try {
+        const skeleton = await createTxSkeleton({ ...txOptions, targets: txOptions.targets.filter((it) => it.to) });
+        setTxSkeleton(skeleton);
+      } catch (e) {
+        setErrorMessage(e.toString());
+      }
+    })();
+  }, [txOptions, privKey]);
 
-        <li>Total capacity: {balance}</li>
-      </ul>
-      <label htmlFor="to-address">Transfer to Address: </label>&nbsp;
-      <input
-        id="to-address"
-        type="text"
-        onChange={(e) => setToAddr(e.target.value)}
-      />
-      <br />
-      <label htmlFor="amount">Amount</label>
-      &nbsp;
-      <input
-        id="amount"
-        type="text"
-        onChange={(e) => setAmount(e.target.value)}
-      />
-      <br />
-      <button
-        onClick={() =>
-          transfer({ amount, from: fromAddr, to: toAddr, privKey })
-        }
-      >
-        Transfer
-      </button>
+  const txFee = useMemo(() => {
+    if (!txSkeleton) return BI.from(0);
+    const outputs = txSkeleton.outputs.reduce((prev, cur) => prev.add(cur.cell_output.capacity), BI.from(0));
+    const inputs = txSkeleton.inputs.reduce((prev, cur) => prev.add(cur.cell_output.capacity), BI.from(0));
+    return inputs.sub(outputs);
+  }, [txSkeleton]);
+
+  const doTransfer = async () => {
+    try {
+      const txHash = await transfer({
+        from: fromAddr,
+        targets: txTargets.map((tx) => ({ to: tx.address, amount: BI.from(tx.amount) })),
+        privKey,
+      });
+      setTxHash(txHash);
+    } catch (e) {
+      setErrorMessage(e.toString());
+    }
+  };
+
+  const txExplorer = useMemo(() => `https://pudge.explorer.nervos.org/transaction/${txHash}`, [txHash]);
+  return (
+    <div className="m-5">
+      <div className="field">
+        <label htmlFor="privateKey" className="label">
+          Private Key
+        </label>
+        <input
+          type="text"
+          onChange={(e) => setPrivKey(e.target.value)}
+          className="input is-primary"
+          placeholder="Your CKB Testnet Private Key"
+        />
+      </div>
+      <div className="box">
+        <div>
+          <strong>CKB Address: </strong> {fromAddr}
+        </div>
+        <div className="mt-2">
+          <strong>Current Lockscript: </strong> {JSON.stringify(fromLock)}
+        </div>
+        <div className="mt-2">
+          <strong>Balance: </strong> {balance} <div className="tag is-info is-light">Shannon</div>
+        </div>
+      </div>
+      <table className="table table is-fullwidth">
+        <thead>
+          <tr>
+            <th>Transfer Address</th>
+            <th>Amount</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {txTargets.map((txTarget, index) => (
+            <tr key={txTarget.key}>
+              <td>
+                <input
+                  type="text"
+                  value={txTarget.address}
+                  onChange={setTargetByIndex(index, "address")}
+                  className="input"
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  value={txTarget.amount as string}
+                  onChange={setTargetByIndex(index, "amount")}
+                  className="input"
+                />
+              </td>
+              <td>
+                {txTargets.length > 1 && (
+                  <button onClick={removeTxTarget(index)} className="button is-danger">
+                    Remove
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>
+              <div className="button" onClick={insertTxTarget}>
+                Add New Transfer Target
+              </div>
+            </th>
+            <th>Transaction fee {txFee.toBigInt().toString()}</th>
+            <th>
+              <button className="button is-primary" onClick={doTransfer}>
+                Transfer!
+              </button>
+            </th>
+          </tr>
+        </tfoot>
+      </table>
+
+      {txHash && (
+        <div className="notification is-primary">
+          <button className="delete" onClick={() => setTxHash("")} />
+          Transaction created, View it on{" "}
+          <a target="_blank" href={txExplorer}>
+            👉CKB Explorer
+          </a>
+        </div>
+      )}
+      {errorMessage && (
+        <div className="notification is-danger">
+          <button className="delete" onClick={() => setErrorMessage("")} />
+          {errorMessage}
+        </div>
+      )}
     </div>
   );
 }
+
+// prevent can not find DOM element on Codesandbox
+const el = document.getElementById("root") || document.createElement("div");
+el.id = "root";
+document.body.appendChild(el);
+
+ReactDOM.render(<App />, el);
