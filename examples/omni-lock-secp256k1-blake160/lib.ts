53db78410aa805544d7f2f22c580162effffb741
zhangyouxin <zhangyouxingg@126.com>
2022-09-14 23:05:21 +0800

    chore: use sealTransaction to put signature to witnesses

--- examples/omni-lock-secp256k1-blake160/lib.ts
+++ examples/omni-lock-secp256k1-blake160/lib.ts
@@ -1,5 +1,6 @@
-import { bytes } from '@ckb-lumos/codec';
-import { blockchain } from '@ckb-lumos/base'
+import { Transaction } from "@ckb-lumos/base";
+import { bytes } from "@ckb-lumos/codec";
+import { blockchain } from "@ckb-lumos/base";
 import { BI, config, helpers, Indexer, RPC, commons, hd, Hash } from "@ckb-lumos/lumos";
 
 export const CONFIG = config.predefined.AGGRON4;
@@ -21,38 +22,37 @@ export interface TransferOptions {
 export async function buildTransfer(options: TransferOptions) {
   let txSkeleton = helpers.TransactionSkeleton({ cellProvider: indexer });
   const fromScript = helpers.parseAddress(options.from);
-  const fromAddress = helpers.encodeToAddress(fromScript, {config: CONFIG});
+  const fromAddress = helpers.encodeToAddress(fromScript, { config: CONFIG });
   const toScript = helpers.parseAddress(options.to);
-  const toAddress = helpers.encodeToAddress(toScript, {config: CONFIG});
+  const toAddress = helpers.encodeToAddress(toScript, { config: CONFIG });
   txSkeleton = await commons.common.transfer(
     txSkeleton,
     [fromAddress],
     toAddress,
-    BigInt(70*10**8),
+    BigInt(70 * 10 ** 8),
     undefined,
     undefined,
-    {config: CONFIG}
-  )
-  txSkeleton = await commons.common.payFee(txSkeleton,  [fromAddress], 1000, undefined, {config: CONFIG})
+    { config: CONFIG }
+  );
+  txSkeleton = await commons.common.payFee(txSkeleton, [fromAddress], 1000, undefined, { config: CONFIG });
   return txSkeleton;
 }
 
 export async function signByPrivateKey(txSkeleton: helpers.TransactionSkeletonType, privateKey: string) {
-  txSkeleton =  commons.common.prepareSigningEntries(txSkeleton)
+  txSkeleton = commons.common.prepareSigningEntries(txSkeleton);
   const message = txSkeleton.get("signingEntries").get(0)!.message;
   const signature = hd.key.signRecoverable(message, privateKey);
-  const signedWitness = bytes.hexify(blockchain.WitnessArgs.pack({
-    lock: commons.omnilock.OmnilockWitnessLock.pack({
+  const packedSignature = bytes.hexify(
+    commons.omnilock.OmnilockWitnessLock.pack({
       signature: signature,
-    }),
-  }))
-  txSkeleton = txSkeleton.update("witnesses", (witnesses) => witnesses.set(0, signedWitness));
-  return txSkeleton;
+    })
+  );
+  const signedTx = helpers.sealTransaction(txSkeleton, [packedSignature]);
+  return signedTx;
 }
 
-export async function sendTransaction(tx: helpers.TransactionSkeletonType): Promise<Hash> {
-  const signedTx = helpers.createTransactionFromSkeleton(tx);
-  return rpc.sendTransaction(signedTx, 'passthrough');
+export async function sendTransaction(tx: Transaction): Promise<Hash> {
+  return rpc.sendTransaction(tx, "passthrough");
 }
 
 export async function capacityOf(address: string): Promise<BI> {
