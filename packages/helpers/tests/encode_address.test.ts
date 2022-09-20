525023dbc69b0177c881cec8a173303697ffc600
Yonghui Lin <homura.dev@gmail.com>
2022-09-20 15:36:21 +0800

    feat(helpers): parse address via known config key (#418)

--- packages/helpers/tests/encode_address.test.ts
+++ packages/helpers/tests/encode_address.test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { encodeToAddress } from "../src";
+import { encodeToAddress, encodeToConfigAddress } from "../src";
 import { predefined } from "@ckb-lumos/config-manager";
 import { Script } from "@ckb-lumos/base";
 
@@ -37,4 +37,25 @@ test("encode to full address, data1", (t) => {
     testnetAddress,
     "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsq4nnw7qkdnnclfkg59uzn8umtfd2kwxceqkkxdwn"
   );
+});
+
+test("encode to address via config key", (t) => {
+  const testnetAddress = encodeToConfigAddress(
+    "0x159890a7cacb44a95bef0743064433d763de229c",
+    "SECP256K1_BLAKE160",
+    { config: AGGRON }
+  );
+
+  t.is(
+    testnetAddress,
+    "ckt1qzda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xwsqg4nzg20jktgj54hmc8gvrygv7hv00z98qklce9w"
+  );
+
+  t.throws(() => {
+    encodeToConfigAddress(
+      "0xb39bbc0b3673c7d36450bc14cfcdad2d559c6c64",
+      "UNKNOWN_LOCK" as any,
+      { config: AGGRON }
+    );
+  });
 });
