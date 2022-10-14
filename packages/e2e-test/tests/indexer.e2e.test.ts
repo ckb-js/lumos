1ce2fe3083db036cc3a656621c367e3824ac7108
painterpuppets <painterpuppets@gmail.com>
2022-10-14 16:03:46 +0800

    feat(ckb-indexer): integrate merged rpc

--- packages/e2e-test/tests/indexer.e2e.test.ts
+++ packages/e2e-test/tests/indexer.e2e.test.ts
@@ -1,6 +1,6 @@
 import anyTest, { TestInterface } from "ava";
 import { generateHDAccount, HDAccount } from "../src/utils";
-import { CKB_RPC_URL, INDEXER_RPC_URL } from "../src/constants";
+import { CKB_RPC_URL } from "../src/constants";
 import { E2EProvider } from "../src/e2eProvider";
 import { join } from "path";
 import { FileFaucetQueue } from "../src/faucetQueue";
@@ -22,7 +22,7 @@ interface TestContext {
 const test = anyTest as TestInterface<TestContext>;
 
 const rpc = new RPC(CKB_RPC_URL);
-const indexer = new Indexer(INDEXER_RPC_URL, CKB_RPC_URL);
+const indexer = new Indexer(CKB_RPC_URL);
 
 const faucetQueue = new FileFaucetQueue(join(__dirname, "../tmp/"));
 const e2eProvider = new E2EProvider({ indexer, rpc, faucetQueue: faucetQueue });
