4771e41636bd11134c070851b63836b98863eeed
布偶什么的就是布偶 <painterpuppets@gmail.com>
2022-11-29 12:00:20 +0800

    fix: light-client get_transactions rpc response is inconsistent (#465)

--- packages/light-client/src/type.ts
+++ packages/light-client/src/type.ts
@@ -26,6 +26,34 @@ export type FetchTransactionResult =
   | { status: FetchFlag.Added; timestamp: string }
   | { status: FetchFlag.NotFound };
 
+export interface LightClientTransactionList<Grouped extends boolean = false> {
+  lastCursor: string | undefined;
+  objects: LightClientTransaction<Grouped>[];
+}
+
+export type LightClientTransaction<Goruped extends boolean = false> =
+  Goruped extends true
+    ? GroupedLightClientTransaction
+    : UngroupedLightClientTransaction;
+
+export type HexNum = string;
+export type IOType = "input" | "output" | "both";
+
+export type UngroupedLightClientTransaction = {
+  transaction: Transaction;
+  blockNumber: HexNum;
+  ioIndex: HexNum;
+  ioType: IOType;
+  txIndex: HexNum;
+};
+
+export type GroupedLightClientTransaction = {
+  transaction: Transaction;
+  blockNumber: HexNum;
+  txIndex: HexNum;
+  cells: Array<[IOType, HexNum]>;
+};
+
 export type LightClientScript = {
   script: Script;
   blockNumber: HexNumber;
