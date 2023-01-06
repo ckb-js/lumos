93d7f7e7a5e9454911a902be3f0fc7e96e8d3125
Yonghui Lin <homura.dev@gmail.com>
2023-01-06 16:46:18 +0800

    chore(codec): outdated example (#472)

--- packages/codec/examples/custom-codec-utf8string.ts
+++ packages/codec/examples/custom-codec-utf8string.ts
@@ -1,9 +1,9 @@
-import { molecule } from "../src";
+import { molecule, bytes } from "../src";
 import { Buffer } from "buffer"; // https://github.com/feross/buffer
 
 const UTF8String = molecule.byteVecOf<string>({
   pack: (str) => Buffer.from(str, "utf8"),
-  unpack: (buf) => Buffer.from(buf).toString("utf8"),
+  unpack: (buf) => Buffer.from(bytes.bytify(buf)).toString("utf8"),
 });
 
 const packed = UTF8String.pack("hello world, 你好世界");
