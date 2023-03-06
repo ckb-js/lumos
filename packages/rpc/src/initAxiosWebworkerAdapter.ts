f33a559311bb7fe2da029242da77360ffee00b13
IronLu233 <ironlu233.dev@outlook.com>
2023-03-06 22:29:43 +0800

    feat(rpc): add unit test for fetch adapter

--- packages/rpc/src/initAxiosWebworkerAdapter.ts
+++ packages/rpc/src/initAxiosWebworkerAdapter.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-// TODO: this is a hack for passing unit tests
-// And a test for here, when the environment is webworker, is needed
+// this is a hack for passing unit tests
 export function initAxiosWebworkerAdapter(): void {
-  /* istanbul ignore if */
   if (
     "ServiceWorkerGlobalScope" in globalThis &&
     globalThis instanceof globalThis?.ServiceWorkerGlobalScope
