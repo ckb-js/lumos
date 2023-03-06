5db6d1cccba070ab33108e8a63a3898eef56a702
IronLu233 <ironlu233.dev@outlook.com>
2023-03-06 23:24:52 +0800

    feat(rpc): fix deploy error

--- packages/rpc/src/initAxiosWebworkerAdapter.ts
+++ packages/rpc/src/initAxiosWebworkerAdapter.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import axios from "axios";
 
 // this is a hack for passing unit tests
+/** @internal */
 export function initAxiosWebworkerAdapter(): void {
   if (
+    // @ts-ignore
     "ServiceWorkerGlobalScope" in globalThis &&
+    // @ts-ignore
     globalThis instanceof globalThis?.ServiceWorkerGlobalScope
   ) {
     /* istanbul ignore next */ const fetchAdapter = require("@vespaiach/axios-fetch-adapter");
