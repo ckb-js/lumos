84fd46c3dbd0f1cf094ba3ef15014f89b8cd4df2
zhangyouxin <zhangyouxingg@126.com>
2023-05-04 11:06:55 +0800

    test: add test case for filter cells

--- packages/ckb-indexer/tests/ckbIndexerFilters.unit.test.ts
+++ packages/ckb-indexer/tests/ckbIndexerFilters.unit.test.ts
@@ -1,6 +1,7 @@
 import test from "ava";
-import { filterBy } from "../src/ckbIndexerFilter";
+import { filterBy, filterByQueryOptions } from "../src/ckbIndexerFilter";
 import { Cell, Script } from "@ckb-lumos/base";
+import { BI } from "@ckb-lumos/bi";
 
 const dummyScript: Script = {
   codeHash: `0x${"00".repeat(32)}`,
@@ -97,3 +98,110 @@ test("should filterBy function filters cell with searchKey", async (t) => {
     })
   );
 });
+
+test("should filterByQueryOptions return expected cells", async (t) => {
+  const originalCells: Cell[] = [
+    createCell({ dataLen: 10 }),
+    createCell({ capacity: BI.from(100) }),
+    createCell({ blockNumber: BI.from(200) }),
+    createCell({ type: dummyScript }),
+  ];
+
+  // must provide lock or type in query options
+  t.throws(() => filterByQueryOptions(originalCells, {}));
+
+  const cells1 = filterByQueryOptions(originalCells, {
+    lock: {
+      script: dummyScript,
+      searchMode: "exact",
+    },
+  });
+  t.deepEqual(cells1, originalCells);
+
+  const cells2 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    outputDataLenRange: [BI.from(10).toHexString(), BI.from(200).toHexString()],
+  });
+  t.deepEqual(cells2, [originalCells[0]]);
+
+  const cells3 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    outputCapacityRange: [
+      BI.from(10).toHexString(),
+      BI.from(200).toHexString(),
+    ],
+  });
+  t.deepEqual(cells3, [originalCells[1]]);
+
+  const cells4 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    fromBlock: BI.from(10).toHexString(),
+  });
+  t.deepEqual(cells4, [originalCells[2]]);
+
+  const cells5 = filterByQueryOptions(originalCells, {
+    type: dummyScript,
+  });
+  t.deepEqual(cells5, [originalCells[3]]);
+
+  const cells6 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    scriptLenRange: [BI.from(100).toHexString(), BI.from(200).toHexString()],
+  });
+  t.deepEqual(cells6, []);
+
+  const cells7 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    data: "0x00000000000000000000",
+  });
+  t.deepEqual(cells7, [originalCells[0]]);
+
+  const cells8 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    data: "0x00",
+  });
+  t.deepEqual(cells8, [originalCells[0]]);
+
+  const cells9 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    data: {
+      data: "0x00",
+      searchMode: "prefix",
+    },
+  });
+  t.deepEqual(cells9, [originalCells[0]]);
+
+  const cells10 = filterByQueryOptions(originalCells, {
+    lock: dummyScript,
+    data: {
+      data: "0x00",
+      searchMode: "exact",
+    },
+  });
+  t.deepEqual(cells10, []);
+
+  const cells11 = filterByQueryOptions(originalCells, {
+    type: dummyScript,
+    scriptSearchMode: "exact",
+  });
+  t.deepEqual(cells11, [originalCells[3]]);
+});
+
+function createCell(payload: {
+  lock?: Script;
+  type?: Script;
+  dataLen?: number;
+  capacity?: BI;
+  blockNumber?: BI;
+}): Cell {
+  const { lock, type, dataLen, capacity, blockNumber } = payload;
+  return {
+    blockNumber: blockNumber?.toHexString() || "0x1",
+    cellOutput: {
+      capacity: capacity?.toHexString() || "0x1",
+      lock: lock || dummyScript,
+      type: type || undefined,
+    },
+    data: "0x" + "00".repeat(dataLen || 0),
+  };
+}
