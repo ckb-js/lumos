3a24d5afe586046e1fe848eb7fbc0bb64497d2a2
IronLu233 <ironlu233.dev@outlook.com>
2023-05-14 18:28:10 +0800

    feat(rpc): update rpc methods

--- packages/rpc/src/Base/indexer.ts
+++ packages/rpc/src/Base/indexer.ts
@@ -1,7 +1,14 @@
 import { formatter as paramsFmts } from "../paramsFormatter";
 import * as resultFmts from "../resultFormatter";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-export default {
+type MethodSchema = {
+  method: string;
+  paramsFormatters: ((arg: any) => any)[];
+  resultFormatters: (arg: any) => any;
+};
+
+const schemas: Record<string, MethodSchema> = {
   getIndexerTip: {
     method: "get_indexer_tip",
     paramsFormatters: [],
@@ -35,4 +42,58 @@ export default {
     paramsFormatters: [paramsFmts.toSearchKey],
     resultFormatters: resultFmts.toCellsCapacity,
   },
+
+  getBlockFilter: {
+    method: "get_block_filter",
+    paramsFormatters: [paramsFmts.toHash],
+    resultFormatters: resultFmts.toNullable(resultFmts.toBlockFilter),
+  },
+  getTransactionAndWitnessProof: {
+    method: "get_transaction_and_witness_proof",
+    paramsFormatters: [
+      paramsFmts.toArray(paramsFmts.toHash),
+      paramsFmts.toOptional(paramsFmts.toHash),
+    ],
+    resultFormatters: resultFmts.toTransactionAndWitnessProof,
+  },
+  verifyTransactionAndWitnessProof: {
+    method: "verify_transaction_and_witness_proof",
+    paramsFormatters: [paramsFmts.toTransactionAndWitnessProof],
+    resultFormatters: resultFmts.toArray(resultFmts.toHash),
+  },
+
+  getForkBlock: {
+    method: "get_fork_block",
+    paramsFormatters: [
+      paramsFmts.toHash,
+      paramsFmts.toOptional(paramsFmts.toNumber),
+    ],
+    resultFormatters: resultFmts.toNullable(resultFmts.toForkBlockResult),
+  },
+
+  getBlockMedianTime: {
+    method: "get_block_median_time",
+    paramsFormatters: [paramsFmts.toHash],
+    resultFormatters: resultFmts.toNullable(resultFmts.toNumber),
+  },
+
+  estimateCycles: {
+    method: "estimate_cycles",
+    paramsFormatters: [paramsFmts.toRawTransaction],
+    resultFormatters: resultFmts.toEstimateCycles,
+  },
+
+  getFeeRateStatistics: {
+    method: "get_fee_rate_statistics",
+    paramsFormatters: [],
+    resultFormatters: resultFmts.toNullable(resultFmts.toFeeRateStatistics),
+  },
+
+  getFeeRateStatics: {
+    method: "get_fee_rate_statics",
+    paramsFormatters: [],
+    resultFormatters: resultFmts.toNullable(resultFmts.toFeeRateStatistics),
+  },
 };
+
+export default schemas;
