f2c736fbf312224f2a80547457e5cbb61cafc555
zhangyouxin <zhangyouxingg@126.com>
2023-10-09 00:09:41 +0800

    refactor: remove peerDependency of toolkit

---
+++ packages/toolkit/tests/normailizers.js
@@ -0,0 +1,10 @@
+const test = require("ava");
+const { normalizers } = require("../lib");
+
+test("correct outPoint should pass validation", (t) => {
+  normalizers.NormalizeOutPoint({
+    txHash: `0x${'00'.repeat(32)}`,
+    index: '0x1',
+  });
+  t.pass();
+});
