6a55b219635a7379cc7416ad471428141f08514c
zhangyouxin <zhangyouxingg@126.com>
2023-10-11 08:36:39 +0800

    chore: lint

--- packages/toolkit/tests/normailizers.js
+++ packages/toolkit/tests/normailizers.js
@@ -3,8 +3,8 @@ const { normalizers } = require("../lib");
 
 test("correct outPoint should pass validation", (t) => {
   normalizers.NormalizeOutPoint({
-    txHash: `0x${'00'.repeat(32)}`,
-    index: '0x1',
+    txHash: `0x${"00".repeat(32)}`,
+    index: "0x1",
   });
   t.pass();
 });
