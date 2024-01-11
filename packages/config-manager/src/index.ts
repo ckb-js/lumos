d08937c7d3b348e898852e20ee2eb71de0c67f14
homura <homura.dev@gmail.com>
2024-01-11 12:43:53 +0800

    feat(config-manager): deprecate the confused name helpers

--- packages/config-manager/src/index.ts
+++ packages/config-manager/src/index.ts
@@ -1,5 +1,9 @@
 export * from "./types";
 export { initializeConfig, getConfig, validateConfig } from "./manager";
+/**
+ * @deprecated use the {@link nameOfScript} and {@link findConfigByScript} function instead
+ */
 export * as helpers from "./helpers";
+export { nameOfScript, findConfigByScript } from "./helpers";
 export { predefined, createConfig } from "./predefined";
 export { generateGenesisScriptConfigs } from "./genesis";
