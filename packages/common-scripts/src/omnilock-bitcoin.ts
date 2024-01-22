999d9dcb1c7d5e3ea5c4b958f7cc73df3e114fb1
homura <homura.dev@gmail.com>
2024-01-22 11:36:52 +0800

    chore(common-scripts): update comment for magic prefix

--- packages/common-scripts/src/omnilock-bitcoin.ts
+++ packages/common-scripts/src/omnilock-bitcoin.ts
@@ -2,7 +2,7 @@ import { bytes, BytesLike } from "@ckb-lumos/codec";
 import { bech32 } from "bech32";
 import bs58 from "bs58";
 
-// https://github.com/XuJiandong/ckb-production-scripts/blob/f884d97963ad553b91bfcc992f68d1ad90f9b244/c/ckb_identity.h#L28
+// https://github.com/cryptape/omnilock/blob/9419b7795641da0ade25a04127e25d8a0b709077/c/ckb_identity.h#L28
 const BTC_PREFIX = "CKB (Bitcoin Layer) transaction: 0x";
 
 /**
