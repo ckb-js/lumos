beb9d590de81bd4dace92e40ba5497c821f30ac7
homura <homura.dev@gmail.com>
2024-01-22 10:46:45 +0800

    chore(examples): outdated api

--- examples/misc/config-manager.ts
+++ examples/misc/config-manager.ts
@@ -6,13 +6,10 @@ const script = {
   args: "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
 };
 
-/* DEPRECATED: initialize config via env mainnet */
-console.log("deprecated initialize", helpers.generateAddress(script));
-
 /* initialize config via AGGRON testnet */
 config.initializeConfig(config.predefined.AGGRON4);
-console.log("testnet address is:", helpers.generateAddress(script));
+console.log("testnet address is:", helpers.encodeToAddress(script));
 
 /* initialize config via LINA testnet */
 config.initializeConfig(config.predefined.LINA);
-console.log("mainnet address is:", helpers.generateAddress(script));
+console.log("mainnet address is:", helpers.encodeToAddress(script));
