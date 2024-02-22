7c3f7a9fd6f31e8ae509d66d203ecf292f17a411
homura <homura.dev@gmail.com>
2024-02-22 18:53:28 +0800

    chore: example for molecule codegen

---
+++ examples/molecule-codegen/main.ts
@@ -0,0 +1,7 @@
+import { Script } from "./generated";
+
+const packed = Script.pack({ codeHash: new Uint8Array(32), hashType: "type", args: "0xdeadbeef" });
+const unpacked = Script.unpack(packed);
+
+console.log("packed script", packed);
+console.log("unpacked script", unpacked);
