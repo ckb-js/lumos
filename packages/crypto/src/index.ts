cd0b0dc69180cf1c5494d0967ee1450e17ed2dec
Tom Wang <tom.wanghaiyi@gmail.com>
2024-06-13 15:38:26 +0800

    build: remove webpack config in nextjs example

--- packages/crypto/src/index.ts
+++ packages/crypto/src/index.ts
@@ -3,7 +3,7 @@ export { hmac } from "@noble/hashes/hmac";
 export { sha256 } from "@noble/hashes/sha256";
 export { sha512 } from "@noble/hashes/sha512";
 export { ripemd160 } from "@noble/hashes/ripemd160";
-export { scrypt, ScryptOpts } from "@noble/hashes/scrypt";
 export { pbkdf2, pbkdf2Async } from "@noble/hashes/pbkdf2";
 export { keccak_256 as keccak256 } from "@noble/hashes/sha3";
+export { scrypt, type ScryptOpts } from "@noble/hashes/scrypt";
 export { randomBytes } from "@noble/hashes/utils";
